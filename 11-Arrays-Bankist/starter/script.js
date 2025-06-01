57be4a91b95cb9283d68f5a945d3227a36fff7f2
Sondos Amr <sondos.amr444@gmail.com>
2025-06-01 03:16:31 +0300

    Overview of key JavaScript array methods: slice (non-destructive copy and subarrays), splice (destructive removal), and at (modern indexing including negative values)

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,28 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+//  slice method
+
+const arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.slice(2)); // it make new array
+console.log(arr.slice(1, 3)); // start parmeter and end note: end parmeter not include the last index
+console.log(arr.slice(-3, -1)); // u can use negative value
+console.log(arr.slice(1, -2)); // u can custom between positive and  nagative parmeters
+console.log(arr.slice()); // shallo it means copy
+console.log([...arr]); // cope with spread operator
+
+// splice method
+console.log('splice method');
+console.log(arr.splice(0, 2)); // it return select ele or deleted ele in new array
+console.log(arr); // print orignal array without slice ele or delete ele
+
+// at method
+console.log(arr[0]);
+console.log(arr.at(0));
+
+console.log(arr[-1]); // it is not work the result is undefind
+console.log(arr[arr.length - 1]); // it work
+console.log(arr.at(-1));
+// u can used with string like this ;
+console.log('sondos'.at(0));
