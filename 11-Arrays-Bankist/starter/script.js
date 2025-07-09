1fe729f2276161f05826c9bc76037f26fc662d88
Sondos Amr <sondos.amr444@gmail.com>
2025-07-09 21:55:23 +0300

    Section 11 - Challenge 2 : Add checkDogs2 to filter Julia's data and classify dog ages

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1127,7 +1127,6 @@ const checkDogs2 = function (dogsJulia, dogsKate) {
   juliaDogs.splice(0, 1);
   juliaDogs.splice(-2);
   const allDods = juliaDogs.concat(dogsKate);
-  console.log(allDods);
   allDods.forEach((dogAge, i) => {
     if (dogAge >= 3) {
       console.log(
@@ -1139,3 +1138,34 @@ const checkDogs2 = function (dogsJulia, dogsKate) {
   });
 };
 checkDogs2([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs2([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+*1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+
+*2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+
+*3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+const calcAverageHumanAge2 = function (dogsAge) {
+  const humanAge = dogsAge
+    .map(dogAge => (dogAge <= 2 ? dogAge * 2 : 16 + dogAge * 4))
+    .filter(dog => dog >= 18);
+  const average = humanAge.reduce((acc, dog) => acc + dog, 0) / humanAge.length;
+  return average;
+};
+console.log(calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge2([16, 6, 10, 5, 6, 1, 4]));
