161f6970a7721d52d5cb63cb551b9436b949d6b1
Sondos Amr <sondos.amr444@gmail.com>
2025-08-11 05:34:26 +0300

    Add cookie message feature, update CSS for button spacing, and add console logs for testing

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const body = document.querySelector('body');
+const againBtn = document.querySelector('.again');
+const num = document.querySelector('.number');
+const guessInput = document.querySelector('.guess');
+const checkBtn = document.querySelector('.check');
+const message = document.querySelector('.message');
+const scoreNum = document.querySelector('.score');
+const highScore = document.querySelector('.highscore');
+
+// <- RANDOM NUM >
+const secritNum = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+secritNum(1, 20);
--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,6 +7,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -28,4 +29,32 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+//////////////////////////
+// console.log('test');
+// console.log(document);
+// console.log(document.documentElement);
+// const allBtn = document.querySelectorAll('.section');
+// console.log([...allBtn]);
+// console.log(Array.from(allBtn));
+// const b1 = [...allBtn];
+// const b2 = Array.from(allBtn);
+// console.log(b1 == b2); // false
+// console.log(JSON.stringify(b1) === JSON.stringify(b2)); // true
+
+////////
+// add message before header
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+message.innerHTML = `We use cookied for improved functionality and analytics.<button class="btn btn--close-cookie">Got it!</button>`;
+
+header.before(message);
+
+const btnCloseCookie = document.querySelector('.btn--close-cookie');
+
+btnCloseCookie.addEventListener('click', function () {
+  message.remove();
 });
