0584a9ffa3a712855bbff5490ff119a2f40dfe9e
Sondos Amr <sondos.amr444@gmail.com>
2025-09-17 02:16:11 +0300

    Implement lazy loading for images using Intersection Observer with rootMargin optimization

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -407,3 +407,25 @@ allSections.forEach(section => {
   obserbver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// Lazy Loading Images
+const allImgs = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${200}px`,
+});
+
+allImgs.forEach(img => imgObserver.observe(img));
