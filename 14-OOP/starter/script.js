6f1a18d7e8510b1f27aa75e2319773c629f2fef0
Sondos Amr <sondos.amr444@gmail.com>
2025-09-29 03:46:18 +0300

    Practice: using prototypes with constructor function

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -21,3 +21,37 @@ console.log(p1 instanceof Person);
 
 const p3 = 'hi';
 console.log(p3 instanceof Person);
+
+// Prototype
+
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  return 2025 - this.myBirthYear;
+};
+
+console.log(Person.prototype);
+
+const p4 = new Person('Sondos', 2020);
+console.log(p4);
+console.log(p4.calcAge());
+console.log(p1.calcAge());
+console.log(p2.calcAge());
+
+console.log(p1.__proto__);
+console.log(p1.__proto__ === Person.prototype);
+
+console.log('==============');
+
+console.log(Person.prototype.isPrototypeOf(p1));
+console.log(Person.prototype.isPrototypeOf(p2));
+console.log(Person.prototype.isPrototypeOf(p3));
+console.log(Person.prototype.isPrototypeOf(p4));
+
+Person.prototype.city = 'Cairo';
+
+console.log(p1);
+console.log(p1.city);
+
+console.log(p1.hasOwnProperty('myName'));
+console.log(p1.hasOwnProperty('city'));
