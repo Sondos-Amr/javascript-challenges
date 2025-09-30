94cfa8ab569e41e2484af496f486d9ad84c1512d
Sondos Amr <sondos.amr444@gmail.com>
2025-10-01 01:53:08 +0300

    Add Student constructor inheriting from Person with introduce method

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,81 +1,81 @@
 'use strict';
 
-function Person(fullName, birthYear) {
-  this.myfullName = fullName;
-  this.myBirthYear = birthYear;
-  // bad way :
-  // this.calcAge = function(){
-  //   console.log(2025 - this.myBirthYear)
-  // }
-}
+// function Person(fullName, birthYear) {
+//   this.myfullName = fullName;
+//   this.myBirthYear = birthYear;
+//   // bad way :
+//   // this.calcAge = function(){
+//   //   console.log(2025 - this.myBirthYear)
+//   // }
+// }
 
-const p1 = new Person('Sondos', 2001);
-console.log(Person);
-console.log('==============');
-console.log(p1);
-console.log('==============');
-const p2 = new Person('Abrar', 2014);
-console.log(p2);
+// const p1 = new Person('Sondos', 2001);
+// console.log(Person);
+// console.log('==============');
+// console.log(p1);
+// console.log('==============');
+// const p2 = new Person('Abrar', 2014);
+// console.log(p2);
 
-console.log(p1 instanceof Person);
+// console.log(p1 instanceof Person);
 
-const p3 = 'hi';
-console.log(p3 instanceof Person);
+// const p3 = 'hi';
+// console.log(p3 instanceof Person);
 
 // Prototype
 
-console.log(Person.prototype);
+// console.log(Person.prototype);
 
-Person.prototype.calcAge = function () {
-  return 2025 - this.myBirthYear;
-};
+// Person.prototype.calcAge = function () {
+//   return 2025 - this.myBirthYear;
+// };
 
-console.log(Person.prototype);
+// console.log(Person.prototype);
 
-const p4 = new Person('Sondos', 2020);
-console.log(p4);
-console.log(p4.calcAge());
-console.log(p1.calcAge());
-console.log(p2.calcAge());
+// const p4 = new Person('Sondos', 2020);
+// console.log(p4);
+// console.log(p4.calcAge());
+// console.log(p1.calcAge());
+// console.log(p2.calcAge());
 
-console.log(p1.__proto__);
-console.log(p1.__proto__ === Person.prototype);
+// console.log(p1.__proto__);
+// console.log(p1.__proto__ === Person.prototype);
 
-console.log('==============');
+// console.log('==============');
 
-console.log(Person.prototype.isPrototypeOf(p1));
-console.log(Person.prototype.isPrototypeOf(p2));
-console.log(Person.prototype.isPrototypeOf(p3));
-console.log(Person.prototype.isPrototypeOf(p4));
+// console.log(Person.prototype.isPrototypeOf(p1));
+// console.log(Person.prototype.isPrototypeOf(p2));
+// console.log(Person.prototype.isPrototypeOf(p3));
+// console.log(Person.prototype.isPrototypeOf(p4));
 
-Person.prototype.city = 'Cairo';
+// Person.prototype.city = 'Cairo';
 
-console.log(p1);
-console.log(p1.city);
+// console.log(p1);
+// console.log(p1.city);
 
-console.log(p1.hasOwnProperty('myfullName'));
-console.log(p1.hasOwnProperty('city'));
+// console.log(p1.hasOwnProperty('myfullName'));
+// console.log(p1.hasOwnProperty('city'));
 
-// Object.Prototype
-console.log(p1.__proto__.__proto__);
-console.log(p1.__proto__.__proto__.__proto__);
+// // Object.Prototype
+// console.log(p1.__proto__.__proto__);
+// console.log(p1.__proto__.__proto__.__proto__);
 
-console.dir(Person.prototype.constructor);
+// console.dir(Person.prototype.constructor);
 
-const arr = [4, 55, 5, 55, 88, 3, 4, 88];
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
+// const arr = [4, 55, 5, 55, 88, 3, 4, 88];
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
 
-console.log(arr.__proto__.__proto__);
+// console.log(arr.__proto__.__proto__);
 
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
 
-console.log(arr.unique());
+// console.log(arr.unique());
 
-const h1 = document.querySelector('h1');
-console.dir(x => x + 1);
+// const h1 = document.querySelector('h1');
+// console.dir(x => x + 1);
 
 // Challenge 1 :
 /* 
@@ -241,3 +241,30 @@ ford.accelerate();
 ford.brake();
 ford.speedUS = 50;
 console.log(ford);
+
+// Inheritance Between "Classes": Constructor Functions
+
+const Person = function (fistName, birthYear) {
+  this.fistName = fistName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2025 - this.birthYear);
+};
+
+const p1 = new Person('sondos', 2002);
+
+p1.calcAge();
+
+const Student = function (fistName, birthYear, study) {
+  Person.call(this, fistName, birthYear);
+  this.study = study;
+};
+
+Student.prototype.intrduce = function () {
+  console.log(`My Name is ${this.fistName} and I study in ${this.study}`);
+};
+const student1 = new Student('Abrar', 2014, 'High School');
+
+student1.intrduce();
