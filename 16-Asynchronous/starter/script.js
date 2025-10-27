04e8d8787d0a679b61ce39837ffa62814924f873
Sondos Amr <sondos.amr444@gmail.com>
2025-10-27 09:04:27 +0300

    Showed Callback Hell using nested setTimeout functions

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -55,3 +55,13 @@ const getCountries = function (country) {
 };
 getCountries('egypt');
 getCountries('palestine');
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 second passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+    }, 1000);
+  }, 1000);
+}, 1000);
