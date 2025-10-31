114eca2512da342825ca4f218440fe72a61b1e1e
Sondos Amr <sondos.amr444@gmail.com>
2025-10-31 23:37:57 +0200

    Add timeout simulation and improved messages in lotteryPromise

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -280,8 +280,11 @@ btn.addEventListener('click', function () {
 // way 2
 
 const lotteryPromise = new Promise(function (resolve, reject) {
-  if (Math.random() >= 0.5) resolve('You WIN 😉🎉🎉👏👏');
-  else reject('You lost your money 😢');
+  console.log('Lotter draw is happening 🙌');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) resolve('You WIN 😉🎉🎉👏👏');
+    else reject(new Error('You lost your money 😢'));
+  }, 2000);
 });
 
 lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
